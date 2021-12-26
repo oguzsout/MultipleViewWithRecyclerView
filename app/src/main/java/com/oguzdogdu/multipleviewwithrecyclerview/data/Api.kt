9ff063ab3bc988959f93eb76d32ce29db4569220
Oguzhan Dogdu <oguzd494@gmail.com>
2021-12-26 17:09:26 +0300

    Api interface created

---
+++ app/src/main/java/com/oguzdogdu/multipleviewwithrecyclerview/data/Api.kt
@@ -0,0 +1,13 @@
+package com.oguzdogdu.multipleviewwithrecyclerview.data
+
+import com.oguzdogdu.multipleviewwithrecyclerview.ui.HomeRecyclerViewItem
+import retrofit2.http.GET
+
+interface Api {
+
+    @GET("movies")
+    suspend fun getMovies(): List<HomeRecyclerViewItem.Movie>
+
+    @GET("directors")
+    suspend fun getDirectors(): List<HomeRecyclerViewItem.Director>
+}
