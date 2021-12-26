e8af5c7279b7ec200f4cb30dd3d9b986b5410043
Oguzhan Dogdu <oguzd494@gmail.com>
2021-12-26 17:13:10 +0300

    Hilt setup

---
+++ app/src/main/java/com/oguzdogdu/multipleviewwithrecyclerview/data/AppModule.kt
@@ -0,0 +1,43 @@
+package com.oguzdogdu.multipleviewwithrecyclerview.data
+
+import androidx.viewbinding.BuildConfig
+import com.oguzdogdu.multipleviewwithrecyclerview.util.Constants.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(): Api {
+        return Retrofit.Builder().baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create()).build()
+            .create(Api::class.java)
+    }
+
+    @Singleton
+    @Provides
+    private fun getRetrofitClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor { chain ->
+                chain.proceed(chain.request().newBuilder().also {
+                    it.addHeader("Accept", "application/json")
+                }.build())
+            }.also { client ->
+                if (BuildConfig.DEBUG) {
+                    val logging = HttpLoggingInterceptor()
+                    logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+                    client.addInterceptor(logging)
+                }
+            }.build()
+    }
+}
