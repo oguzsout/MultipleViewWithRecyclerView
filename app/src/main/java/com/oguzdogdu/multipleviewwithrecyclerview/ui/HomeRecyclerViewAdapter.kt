36b1077737c308d6807c096d0d4c444a31ea3fe2
oguzsout <oguzd494@gmail.com>
2022-01-04 18:55:47 +0300

    HomeRecyclerViewAdapter.kt added

---
+++ app/src/main/java/com/oguzdogdu/multipleviewwithrecyclerview/ui/HomeRecyclerViewAdapter.kt
@@ -0,0 +1,67 @@
+package com.oguzdogdu.multipleviewwithrecyclerview.ui
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.oguzdogdu.multipleviewwithrecyclerview.R
+import com.oguzdogdu.multipleviewwithrecyclerview.databinding.ItemDirectorBinding
+import com.oguzdogdu.multipleviewwithrecyclerview.databinding.ItemMovieBinding
+import com.oguzdogdu.multipleviewwithrecyclerview.databinding.ItemTitleBinding
+
+class HomeRecyclerViewAdapter : RecyclerView.Adapter<HomeRecyclerViewHolder>() {
+
+    private var items = listOf<HomeRecyclerViewItem>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+     var itemClickListener: ((view: View, item: HomeRecyclerViewItem, position: Int) -> Unit)? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeRecyclerViewHolder {
+        return when (viewType) {
+            R.layout.item_title -> HomeRecyclerViewHolder.TitleViewHolder(
+                ItemTitleBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+            )
+            R.layout.item_movie -> HomeRecyclerViewHolder.MovieViewHolder(
+                ItemMovieBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+            )
+            R.layout.item_director -> HomeRecyclerViewHolder.DirectorViewHolder(
+                ItemDirectorBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+            )
+            else -> throw IllegalArgumentException("Invalid ViewType Provided")
+        }
+    }
+
+    override fun onBindViewHolder(holder: HomeRecyclerViewHolder, position: Int) {
+        holder.itemClickListener = itemClickListener
+        when (holder) {
+            is HomeRecyclerViewHolder.DirectorViewHolder -> holder.bind(items[position] as HomeRecyclerViewItem.Director)
+            is HomeRecyclerViewHolder.MovieViewHolder -> holder.bind(items[position] as HomeRecyclerViewItem.Movie)
+            is HomeRecyclerViewHolder.TitleViewHolder -> holder.bind(items[position] as HomeRecyclerViewItem.Title)
+        }
+    }
+
+    override fun getItemCount() = items.size
+
+    override fun getItemViewType(position: Int): Int {
+        return when (items[position]) {
+            is HomeRecyclerViewItem.Director -> R.layout.item_director
+            is HomeRecyclerViewItem.Movie -> R.layout.item_movie
+            is HomeRecyclerViewItem.Title -> R.layout.item_title
+        }
+    }
+}
